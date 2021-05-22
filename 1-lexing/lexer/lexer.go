e4f1d7e4b69aece99446960f6ab27bd0bfaa2580
Aulia Widyaputra <aulia.widyaputra@gmail.com>
2021-05-22 14:41:49 +0700

    Add equivalence operators

--- 1-lexing/lexer/lexer.go
+++ 1-lexing/lexer/lexer.go
@@ -22,13 +22,27 @@ func (l *Lexer) NextToken() token.Token {
 
 	switch l.ch {
 	case '=':
-		tok = newToken(token.ASSIGN, l.ch)
+		if l.peekChar() == '=' {
+			ch := l.ch
+			l.readChar()
+			literal := string(ch) + string(l.ch)
+			tok = token.Token{Type: token.EQ, Literal: literal}
+		} else {
+			tok = newToken(token.ASSIGN, l.ch)
+		}
+	case '!':
+		if l.peekChar() == '=' {
+			ch := l.ch
+			l.readChar()
+			literal := string(ch) + string(l.ch)
+			tok = token.Token{Type: token.NOT_EQ, Literal: literal}
+		} else {
+			tok = newToken(token.BANG, l.ch)
+		}
 	case '+':
 		tok = newToken(token.PLUS, l.ch)
 	case '-':
 		tok = newToken(token.MINUS, l.ch)
-	case '!':
-		tok = newToken(token.BANG, l.ch)
 	case '*':
 		tok = newToken(token.ASTERISK, l.ch)
 	case '/':
@@ -118,5 +132,13 @@ func (l *Lexer) skipWhiteSpace() {
 		l.ch == '\n' ||
 		l.ch == '\r' {
 		l.readChar()
+	}
+}
+
+func (l *Lexer) peekChar() byte {
+	if l.readPosition >= len(l.input) {
+		return 0
+	} else {
+		return l.input[l.readPosition]
 	}
 }
